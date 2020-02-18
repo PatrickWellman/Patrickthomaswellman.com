8073d2bc264eb02880fa3f7bb374fb82547602e9
PatrickWellman <pwellmancode@gmail.com>
2020-02-18 15:49:53 -0600

    responsive navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Landing from './Landing';
 import BlogPostList from './BlogPostList';
 import CommentDetail from './CommentDetail';
+import Navbar from './Navbar';
 import ApprovalCard from './ApprovalCard';
 import '../App.css';
 
 const App = () => {
 	return (
 		<div>
+			
 			<Landing />
 			<BlogPostList />
 
