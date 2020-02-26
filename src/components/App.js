a193556b69090f7cbaa7aa4c2ef944f415c54e28
PatrickWellman <pwellmancode@gmail.com>
2020-02-26 11:41:47 -0600

    finished navigation

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,16 @@ import React from 'react';
 import Landing from './Landing';
 import AboutMe from './AboutMe';
 import BlogPostList from './BlogPostList';
-import ImageGrid from './ImageGrid';
-import ImageDisplay from './ImageDisplay';
+
 import Navigation from './Navigation';
 
 import '../App.css';
-
 
 const App = () => {
 	return (
 		<div>
 			<Navigation />
 			<Landing />
-			<ImageGrid />
-
-			<BlogPostList />
 		</div>
 	);
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,29 +7,33 @@ class Navigation extends React.Component {
 			<div className="navigation">
 				<input type="checkbox" className="navigation__checkbox" id="navi__toggle" />
 				<label for="navi__toggle" className="navigation__button">
-					MENU
+					<span className="navigation__icon">MENU</span>
 				</label>
-				<div className="navigation__background">&nbsp;</div>
+				<div className="navigation__background">X</div>
 
 				<nav className="navigation__nav">
 					<ul className="navigation__list">
 						<li className="navigation__item">
 							<a href="#" className="navigation__link">
+								<span className="navListNumber">01</span>
 								crust to crumbs
 							</a>
 						</li>
 						<li className="navigation__item">
 							<a href="#" className="navigation__link">
-								image gallery
+								<span className="navListNumber">02</span>
+								Image gallery
 							</a>
 						</li>
 						<li className="navigation__item">
 							<a href="#" className="navigation__link">
+								<span className="navListNumber">03</span>
 								chef stuff
 							</a>
 						</li>
 						<li className="navigation__item">
 							<a href="#" className="navigation__link">
+								<span className="navListNumber">04</span>
 								resume
 							</a>
 						</li>
