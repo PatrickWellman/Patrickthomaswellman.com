162e7977063cb0e8ae9b4760f781eb6c0b596183
PatrickWellman <pwellmancode@gmail.com>
2020-04-01 08:17:37 -0500

    navbar

--- src/components/LandingComps/NaviBar.js
+++ src/components/LandingComps/NaviBar.js
@@ -1,34 +1,37 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-
+import '../CSS/navibar.css';
 
 class NaviBar extends React.Component {
 	render() {
 		return (
-			<nav className="navigation__nav">
-				<ul className="navigation__list">
-					<li className="navigation__item">
-						<Link to="/photogallery" className="navigation__link" onClick={this.toggleMenu}>
-							photo gallery
-						</Link>
-					</li>
-					<li className="navigation__item">
-						<Link to="/crusttocrumbs" className="navigation__link" onClick={this.toggleMenu}>
-							crust to crumbs
-						</Link>
-					</li>
-					<li className="navigation__item">
-						<Link to="/chefstuff" className="navigation__link" onClick={this.toggleMenu}>
-							chefstuff
-						</Link>
-					</li>
-					<li className="navigation__item">
-						<Link to="/resume" className="navigation__link" onClick={this.toggleMenu}>
-							resume
-						</Link>
-					</li>
-				</ul>
-			</nav>
+			<Container className="navBar">
+				<nav className="navigation__nav">
+					<ul className="navigation__list">
+						<li className="navigation__item">
+							<Link to="/photogallery" className="navigation__link" onClick={this.toggleMenu}>
+								photo gallery
+							</Link>
+						</li>
+						<li className="navigation__item">
+							<Link to="/crusttocrumbs" className="navigation__link" onClick={this.toggleMenu}>
+								crust to crumbs
+							</Link>
+						</li>
+						<li className="navigation__item">
+							<Link to="/chefstuff" className="navigation__link" onClick={this.toggleMenu}>
+								chefstuff
+							</Link>
+						</li>
+						<li className="navigation__item">
+							<Link to="/resume" className="navigation__link" onClick={this.toggleMenu}>
+								resume
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</Container>
 		);
 	}
 }
