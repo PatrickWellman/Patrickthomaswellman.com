8c8697de4bdcf15a01ddca7c22ecbce8e76b6ffe
PatrickWellman <pwellmancode@gmail.com>
2020-04-02 17:55:52 -0500

    about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,10 +14,11 @@ class AboutMe extends React.Component {
 					years. Some of the more notable places I have lived include Zurich, Geneva, Moscow, and Miami Beach.
 				</p>
 				<p>
-					Working as a pastry chef since 2012 was amazing. I met some really great people, was able to learn
-					the craft from an amazing mentor, and a lot about myself along the way. Recently I decided to step
-					out of the kitchen. In mid 2019, I decided to make a carrer change into web development. I still
-					very much enjoy cooking, but prefer to do it at home now as a hobby.
+					Working as a pastry chef since 2012 was, I have been able to work everywhere from 5 star resorts to
+					20 course tasting menu restaurants. I met some really great people, was able to learn the craft from
+					an amazing mentor, and a lot about myself along the way. Recently I decided to step out of the
+					kitchen. In mid 2019, I decided to make a carrer change into web development. I still very much
+					enjoy cooking, but prefer to do it at home now as a hobby.
 				</p>
 				<p>
 					I've recently relocated to the Los Angeles area from Nashville to be closer to the heart of the tech
