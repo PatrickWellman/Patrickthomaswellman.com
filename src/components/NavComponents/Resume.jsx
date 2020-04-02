ae6d5da61634cd0bff8fa600e67c29113531e9dc
PatrickWellman <pwellmancode@gmail.com>
2020-04-01 19:05:02 -0500

    added resume

--- src/components/NavComponents/Resume.jsx
+++ src/components/NavComponents/Resume.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import resume from './Patrick Resume.pdf';
 
 // import resume from '../Rohan Damani Resume 2018.pdf';
 
-const Resume = () => <div>Resume</div>;
+const Resume = () => (
+ <div className="iframe-container">
+    <iframe src={resume} width="100%" height="2200px" className="iframe" />
+</div>
+);
 
 export default Resume;
